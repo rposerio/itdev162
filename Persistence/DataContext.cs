69d811c9ff698bb92e7e01e0cd2d47ee8720acad
rposerio <rposerio@icloud.com>
2020-10-22 15:18:26 -0500

    finish activity 3

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Value> Values { get; set;}
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(
+                new Value { Id = 1, Name = "Value1" },
+                new Value { Id = 2, Name = "Value2" },
+                new Value { Id = 3, Name = "Value3" }
+            );
+        }
+    }
+}
